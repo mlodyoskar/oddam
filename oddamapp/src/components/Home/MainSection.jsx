2eb8de22e2e2c88eefe52b23c303811a55e6ace7
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-01-06 00:26:36 +0100

    pusty html pierwsza sekcja

--- oddamapp/src/components/Home/MainSection.jsx
+++ oddamapp/src/components/Home/MainSection.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import '../../scss/HomeStyle/MainSection.scss'
-import HeroImage from '../../assets/Home-Hero-Image.jpg';
-const MainSection = ()=>{
-    return(
-        <>
-        <section>
-<div>
-    <img src={HeroImage} alt="losowe rzeczy"/>
-</div>
-
-        </section>
-        </>
-    )
-}
+import "../../scss/HomeStyle/MainSection.scss";
+import HeroImage from "../../assets/Home-Hero-Image.jpg";
+import Decoration from "../../assets/Decoration.svg";
+const MainSection = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <img src={HeroImage} alt="losowe rzeczy" />
+        </div>
+        <div>
+            <div>
+                <button>
+                    Zaloguj
+                </button>
+                <button>Załóż konto</button>
+            </div>
+            <nav>
+                <ul>
+                    <li>Start</li>
+                    <li>O co chodzi?</li>
+                    <li>O nas</li>
+                    <li>Fundacja i organizacje</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>
+            <div>
+                <h1>Zacznij pomagać <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
+                <img src={Decoration} alt='decoration' />
+                <div>
+                    <button>Oddaj rzeczy</button>
+                    <button>zorganizuj zbiórkę</button>
+                </div>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default MainSection;
