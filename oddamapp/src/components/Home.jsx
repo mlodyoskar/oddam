5d9ef6977df6d75270ae5f768142e36562300f55
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-01-07 12:41:00 +0100

    czwarta sekcja

--- oddamapp/src/components/Home.jsx
+++ oddamapp/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainSection from "./Home/MainSection";
 import ThreeColumns from "./Home/ThreeColumns";
 import Steps from "./Home/Steps";
+import AboutUs from "./Home/AboutUs";
 
 const Home=()=>{
 return(
@@ -9,6 +10,7 @@ return(
     <MainSection/>
     <ThreeColumns/>
     <Steps/>
+    <AboutUs/>
     </>
 );
 }
