071bf371479a0fd9be50ccb1539478ccf30de2db
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-01-07 20:26:43 +0100

    prawie gotowe komu pomagamy

--- oddamapp/src/components/Home/WhoWeHelp.jsx
+++ oddamapp/src/components/Home/WhoWeHelp.jsx
@@ -1,48 +1,49 @@
 import React from "react";
 import Decoration from "../../assets/Decoration.svg";
+import "../../scss/HomeStyle/whoWeHelp.scss";
 const WhoWeHelp = () => {
   return (
     <>
       <section className="whoWeHelp-section">
-        <div>
+        <div className="whoWeHelp-heading-block">
+            <div>
           <h1 className="whoWeHelp-heading">Komu pomagamy?</h1>
+          </div>
           <img src={Decoration} alt="decoration" />
           <div className="three-btns">
-            <button className="whoWeHelp-btn">Fundacja</button>
+            <button className="whoWeHelp-btn">Fundacjom</button>
             <button className="whoWeHelp-btn">Organizacjom pozarządowym</button>
             <button className="whoWeHelp-btn">Lokalnym zbiórkom</button>
           </div>
           <p className="whoWeHelp-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation.
+          W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
           </p>
           <div className="collection-containers">
             <div className="collection-container">
-              <h1 className="collection-container-heading">Zbiórka “Lorem Ipsum 1”</h1>
-              <div className="collection-block">
+              <h1 className="collection-container-heading">Fundacja “Dbam o Zdrowie"</h1>
+              <div className="collection-block border">
                 <p className="collection-block_description">
-                  Quis varius quam quisque id diam vel quam elementum pulvinar.
+                Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.
                 </p>
-                <p className="collection-block-description2">Egestas, sed, tempus</p>
+                <p className="collection-block-description2">ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
               </div>
             </div>
             <div className="collection-container">
-              <h1 className="collection-container-heading">Zbiórka “Lorem Ipsum 1”</h1>
-              <div className="collection-block">
+              <h1 className="collection-container-heading">Fundacja “Dla dzieci”</h1>
+              <div className="collection-block border">
                 <p className="collection-block_description">
-                  Quis varius quam quisque id diam vel quam elementum pulvinar.
+                Cel i misja: Pomoc dzieciom z ubogich rodzin.
                 </p>
-                <p className="collection-block_description2">Egestas, sed, tempus</p>
+                <p className="collection-block_description2">ubrania, meble, zabawki</p>
               </div>
             </div>
-            <div className="collection-block">
-              <h1 className="collection-container-heading">Zbiórka “Lorem Ipsum 1”</h1>
-              <div>
+            <div className="collection-container">
+              <h1 className="collection-container-heading">Fundacja “Bez domu”</h1>
+              <div className="collection-block" >
                 <p className="collection-block_description">
-                  Quis varius quam quisque id diam vel quam elementum pulvinar.
+                Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.
                 </p>
-                <p className="collection-block_description2">Egestas, sed, tempus</p>
+                <p className="collection-block_description2">ubrania, jedzenie, ciepłe koce</p>
               </div>
             </div>
           </div>
