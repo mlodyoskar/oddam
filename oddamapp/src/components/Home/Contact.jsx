f7a5334de3356089d1ad6d9da8553c6043c33241
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-01-08 23:07:33 +0100

    gotowa strona

--- oddamapp/src/components/Home/Contact.jsx
+++ oddamapp/src/components/Home/Contact.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import BackgroundContact from "../../assets/Background-Contact-Form.jpg";
 import Decoration from "../../assets/Decoration.svg";
 import "../../scss/HomeStyle/contact.scss";
+import Facebook from "../../assets/Facebook.svg";
+import Instagram from "../../assets/Instagram.svg";
 const Contact = () => {
   return (
     <>
@@ -23,7 +25,7 @@ const Contact = () => {
               <div className="row-columns">
                 <label className="two-labels" htmlFor="text">
                   Wpisz swoje imię
-                  <input type="text" placeholder="Krzysztof"/>
+                  <input type="text" placeholder="Krzysztof" />
                 </label>
                 <label className="two-labels" htmlFor="text">
                   Wpisz swój Email
@@ -33,14 +35,28 @@ const Contact = () => {
               <div>
                 <label className="third-label">
                   Wpisz swoją wiadomość
-                  <textarea type="text" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+                  <textarea
+                    type="text"
+                    placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  />
                 </label>
               </div>
-              <button>Wyślij</button>
+              <div className="form-btn-block">
+                <button className="form-btn">Wyślij</button>
+              </div>
             </div>
           </form>
         </div>
       </section>
+      <footer className="footer-block">
+        <div className="Copyright-block">
+          <p>Copyright by Coders Lab</p>
+        </div>
+        <div className="footer-icon-block">
+          <img className="footer-fb-icon" src={Facebook} alt="Facebook icon" />
+          <img src={Instagram} alt="Instagram icon" />
+        </div>
+      </footer>
     </>
   );
 };
