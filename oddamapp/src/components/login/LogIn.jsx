13ddf8f4e0f7512ca5752d936580765edb729d89
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-01-30 14:36:44 +0100

    poprawne logowanie

--- oddamapp/src/components/login/LogIn.jsx
+++ oddamapp/src/components/login/LogIn.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../Home/Navigation";
 import Decoration from "../../assets/Decoration.svg";
 import '../../scss/loginStyle/logIn.scss'
 import { Link as RouterLink } from "react-router-dom";
+import { supabase } from "../../supabase";
 const LogIn = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+    if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
+      setEmailError("Podaj poprawny adres email");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+    if (!password) {
+      setPasswordError("Podaj hasło");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+    return isValid;
+  };
+
+  const handleLogin = async (e) =>{
+    e.preventDefault();
+
+    if(validateForm()){
+try{
+  const {user, error} = await supabase.auth.signInWithPassword({
+    email:email,
+    password:password,
+  });
+  if (error){
+    console.error('Bład logowania:', error.message);
+  }else{
+    console.log('Zalogowano pomyślnie', user);
+  }
+}catch (error){
+  console.error('Bład logowania:', error.message);
+}
+    }else{
+      console.log('Bład walidacji');
+    }
+  };
+  
+
   return (
     <>
       <Navigation/>
@@ -15,16 +62,18 @@ const LogIn = () => {
         <form className="log-form">
           <div className="input-block">
             <label>Email</label>
-            <input type="text"/>
+            <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+            <span className="error-message">{emailError}</span>
             </div>
             <div className="input-block">
             <label htmlFor="password">Hasło</label>
-            <input type="password"/>
+            <input type="password"  value={password} onChange={(e)=> setPassword(e.target.value)}/>
+            <span className="error-message">{passwordError}</span>
             </div>
         </form>
         <div className="log-btn-block">
         <RouterLink to="/registration">  <button className="register-btn">Załóż konto</button> </RouterLink>
-        <button className="log-btn">Zaloguj się </button>
+        <button className="log-btn" onClick={handleLogin}>Zaloguj się </button>
         </div>
       </section>
     </>
